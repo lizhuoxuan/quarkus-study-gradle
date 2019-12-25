305f24f40992b56e53045402b928f035bd145439
lzx <307871680@qq.com>
2019-12-25 16:25:59 +0800

    Initial commit

---
+++ src/native-test/java/com/xzlzx/NativequarkusIT.java
@@ -0,0 +1,9 @@
+package com.xzlzx;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativequarkusIT extends quarkusTest {
+
+    // Execute the same tests but in native mode.
+}
