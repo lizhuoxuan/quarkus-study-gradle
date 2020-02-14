7d2fe9c23452926e0731a812e61f4a8f71505730
lzx <zhuoxuan_li@foxmail.com>
2020-02-14 17:01:46 +0800

    swagger ui

--- src/main/java/com/xzlzx/bean/PostBody.java
+++ src/main/java/com/xzlzx/bean/PostBody.java
@@ -1,9 +1,14 @@
 package com.xzlzx.bean;
 
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+
 public class PostBody {
 
+    // swagger ui 说明
+    @Schema(description = "a的说明")
     public Integer a;
 
+    @Schema(description = "b的说明")
     public String b;
 
     public PostBody() {
--- src/main/java/com/xzlzx/bean/TableUser.java
+++ src/main/java/com/xzlzx/bean/TableUser.java
@@ -3,16 +3,19 @@ package com.xzlzx.bean;
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 
 import javax.persistence.Entity;
+import java.time.LocalDateTime;
 
 @Entity
-public class User extends PanacheEntity {
+public class TableUser extends PanacheEntity {
 
     public String name;
 
-    public User() {
+    public LocalDateTime createTime;
+
+    public TableUser() {
     }
 
-    public User(String name) {
+    public TableUser(String name) {
         this.name = name;
     }
 }
